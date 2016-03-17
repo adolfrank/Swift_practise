fb744ca36ae40c13fb8d23247e0bd651c7b855fc
FrankAdol <frank@adols-macbook-pro.local>
2016-03-18 00:07:27 +0800

    简单秒表

--- 01-day/01-day/ViewController.swift
+++ 01-day/01-day/ViewController.swift
@@ -10,16 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var playBtn: UIButton!
+    @IBOutlet weak var stopBtn: UIButton!
+    @IBOutlet weak var timeLable: UILabel!
+    
+    var Count = 0.0
+    var Timer = NSTimer()
+    
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        timeLable.text = String(Count)
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+  
+    @IBAction func resetBtnClick(sender: AnyObject) {
+        Count = 0.0
+        timeLable.text = String(Count)
+        playBtn.enabled = true
+        stopBtn.enabled = true
     }
 
-
+    @IBAction func playBtnClick(sender: AnyObject) {
+        playBtn.enabled = false
+        stopBtn.enabled = true
+        Timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: ("timeUpdate"), userInfo: nil, repeats: true)
+    }
+    
+    @IBAction func stopBtnClick(sender: AnyObject) {
+        stopBtn.enabled = false
+        playBtn.enabled = true
+        Timer.invalidate()
+    }
+    
+    func timeUpdate(){
+        Count += 0.1
+        timeLable.text = String(format: "%.1f", arguments: [Count])
+    }
+    
+    
+    
+    
 }
-
