2e90444def7e479ae60d61ed29244dda53e83300
adolfrank <adolfrank@outlook.com>
2016-03-25 10:11:14 +0800

    完善框架

--- 11-day/11-day/ProfileViewController.swift
+++ 11-day/11-day/ProfileViewController.swift
@@ -9,11 +9,12 @@
 import UIKit
 
 let offset_HeaderStop:CGFloat = 40.0 // At this offset the Header stops its transformations
-let offset_B_LabelHeader:CGFloat = 95.0 // At this offset the Black label reaches the Header
+let offset_B_LabelHeader:CGFloat = 125.0 // At this offset the Black label reaches the Header
 let distance_W_LabelHeader:CGFloat = 35.0 // The distance between the bottom of the Header and the top of the White Label
 
 class ProfileViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    @IBOutlet weak var backBtn: UIButton!
     @IBOutlet var Header: UIView!
     @IBOutlet weak var HeaderLable: UILabel!
     @IBOutlet weak var AvatarImage: UIImageView!
@@ -28,11 +29,6 @@ class ProfileViewController: UIViewController,UITableViewDelegate,UITableViewDat
         self.view.backgroundColor = UIColor.whiteColor()
         ProfileTable.delegate = self
         ProfileTable.dataSource = self
-        
-        
-           }
-
-    override func viewDidAppear(animated: Bool) {
         // Header - Image
         
         headerImageView = UIImageView(frame: Header.bounds)
@@ -43,13 +39,85 @@ class ProfileViewController: UIViewController,UITableViewDelegate,UITableViewDat
         // Header - Blurred Image
         
         headerBlurImageView = UIImageView(frame: Header.bounds)
-//        headerBlurImageView?.image = UIImage(named: "header_bg")?.blurredImageWithRadius(10, iterations: 20, tintColor: UIColor.clearColor())
+        //        headerBlurImageView?.image = UIImage(named: "header_bg")?.blurredImageWithRadius(10, iterations: 20, tintColor: UIColor.clearColor())
         headerBlurImageView?.contentMode = UIViewContentMode.ScaleAspectFill
         headerBlurImageView?.alpha = 0.0
         Header.insertSubview(headerBlurImageView, belowSubview: HeaderLable)
         Header.clipsToBounds = true
+        
+        }
 
+    @IBAction func backBtnDidTouch(sender: AnyObject) {
+        
+        print("backBtnDidTouch")
     }
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        
+        let offset = scrollView.contentOffset.y
+        var avatarTransform = CATransform3DIdentity
+        var headerTransform = CATransform3DIdentity
+        
+        // PULL DOWN -----------------
+        
+        if offset <= 0 {
+            
+            let headerScaleFactor:CGFloat = -(offset) / Header.bounds.height
+            let headerSizevariation = ((Header.bounds.height * (1.0 + headerScaleFactor)) - Header.bounds.height)/2.0
+            headerTransform = CATransform3DTranslate(headerTransform, 0, headerSizevariation, 0)
+            headerTransform = CATransform3DScale(headerTransform, 1.0 + headerScaleFactor, 1.0 + headerScaleFactor, 0)
+            
+            Header.layer.transform = headerTransform
+        }
+            
+            // SCROLL UP/DOWN ------------
+            
+        else {
+            
+            // Header -----------
+            
+            headerTransform = CATransform3DTranslate(headerTransform, 0, max(-offset_HeaderStop, -offset), 0)
+            
+            //  ------------ Label
+            
+//            let labelTransform = CATransform3DMakeTranslation(0, max(-distance_W_LabelHeader, offset_B_LabelHeader - offset), 0)
+//            HeaderLable.layer.transform = labelTransform
+
+            
+            //  ------------ 返回按钮顶置
+            let backBtnTrasform = CATransform3DMakeTranslation(0, 0, 10)
+            backBtn.layer.transform = backBtnTrasform
+
+            
+            //  ------------ Blur
+            
+            headerBlurImageView?.alpha = min (1.0, (offset - offset_B_LabelHeader)/distance_W_LabelHeader)
+            
+            // Avatar -----------
+            
+            let avatarScaleFactor = (min(offset_HeaderStop, offset)) / AvatarImage.bounds.height / 1.4 // Slow down the animation
+            let avatarSizeVariation = ((AvatarImage.bounds.height * (1.0 + avatarScaleFactor)) - AvatarImage.bounds.height) / 2.0
+            avatarTransform = CATransform3DTranslate(avatarTransform, 0, avatarSizeVariation, 0)
+            avatarTransform = CATransform3DScale(avatarTransform, 1.0 - avatarScaleFactor, 1.0 - avatarScaleFactor, 0)
+            
+            if offset <= offset_HeaderStop {
+                
+                if AvatarImage.layer.zPosition < Header.layer.zPosition{
+                    Header.layer.zPosition = 0
+                }
+                
+            }else {
+                if AvatarImage.layer.zPosition >= Header.layer.zPosition{
+                    Header.layer.zPosition = 2
+                }
+            }
+        }
+        // Apply Transformations
+        Header.layer.transform = headerTransform
+        AvatarImage.layer.transform = avatarTransform
+    }
+    
+
     
     
     override func viewWillAppear(animated: Bool) {
@@ -83,68 +151,6 @@ class ProfileViewController: UIViewController,UITableViewDelegate,UITableViewDat
         return cell
     }
 
-    
-    
-//    func scrollViewDidScroll(scrollView: UIScrollView) {
-//        
-//        let offset = scrollView.contentOffset.y
-//        var avatarTransform = CATransform3DIdentity
-//        var headerTransform = CATransform3DIdentity
-//        
-//        // PULL DOWN -----------------
-//        
-//        if offset < 0 {
-//            
-//            let headerScaleFactor:CGFloat = -(offset) / Header.bounds.height
-//            let headerSizevariation = ((Header.bounds.height * (1.0 + headerScaleFactor)) - Header.bounds.height)/2.0
-//            headerTransform = CATransform3DTranslate(headerTransform, 0, headerSizevariation, 0)
-//            headerTransform = CATransform3DScale(headerTransform, 1.0 + headerScaleFactor, 1.0 + headerScaleFactor, 0)
-//            
-//            Header.layer.transform = headerTransform
-//        }
-//            
-//            // SCROLL UP/DOWN ------------
-//            
-//        else {
-//            
-//            // Header -----------
-//            
-//            headerTransform = CATransform3DTranslate(headerTransform, 0, max(-offset_HeaderStop, -offset), 0)
-//            
-//            //  ------------ Label
-//            
-//            let labelTransform = CATransform3DMakeTranslation(0, max(-distance_W_LabelHeader, offset_B_LabelHeader - offset), 0)
-//            HeaderLable.layer.transform = labelTransform
-//            
-//            //  ------------ Blur
-//            
-//            headerBlurImageView?.alpha = min (1.0, (offset - offset_B_LabelHeader)/distance_W_LabelHeader)
-//            
-//            // Avatar -----------
-//            
-//            let avatarScaleFactor = (min(offset_HeaderStop, offset)) / AvatarImage.bounds.height / 1.4 // Slow down the animation
-//            let avatarSizeVariation = ((AvatarImage.bounds.height * (1.0 + avatarScaleFactor)) - AvatarImage.bounds.height) / 2.0
-//            avatarTransform = CATransform3DTranslate(avatarTransform, 0, avatarSizeVariation, 0)
-//            avatarTransform = CATransform3DScale(avatarTransform, 1.0 - avatarScaleFactor, 1.0 - avatarScaleFactor, 0)
-//            
-//            if offset <= offset_HeaderStop {
-//                
-//                if AvatarImage.layer.zPosition < Header.layer.zPosition{
-//                    Header.layer.zPosition = 0
-//                }
-//                
-//            }else {
-//                if AvatarImage.layer.zPosition >= Header.layer.zPosition{
-//                    Header.layer.zPosition = 2
-//                }
-//            }
-//        }
-//        
-//        // Apply Transformations
-//        
-//        Header.layer.transform = headerTransform
-//        AvatarImage.layer.transform = avatarTransform
-//    }
     
    
 
