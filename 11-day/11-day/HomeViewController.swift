93b69ffd2e5c68384b001f22c8fbe37723ec77cb
adolfrank <adolfrank@outlook.com>
2016-03-25 17:54:04 +0800

    完善MVC模式

--- 11-day/11-day/HomeViewController.swift
+++ 11-day/11-day/HomeViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+
 struct video {
     let image: String
     let title: String
@@ -20,20 +21,7 @@ struct video {
 
 class HomeViewController: UITableViewController,UIGestureRecognizerDelegate {
 
-    
-    
-    var data :[video] = [
-        video(image: "screenshot01", title: "Introduce 3DS Mario", subTitle: "Youtube - 06:32", source: "video01.mp4", avatar: "avatar01"),
-        video(image: "screenshot02", title: "Emoji Among Us", subTitle: "Vimeo - 3:34", source: "video01.mp4", avatar: "avatar02"),
-        video(image: "screenshot03", title: "Seals Documentary", subTitle: "Vine - 00:06", source: "video01.mp4", avatar: "avatar03"),
-        video(image: "screenshot04", title: "Adventure Time", subTitle: "Youtube - 02:39", source: "video01.mp4", avatar: "avatar04"),
-        video(image: "screenshot05", title: "Facebook HQ", subTitle: "Facebook - 10:20", source: "video02.mp4", avatar: "avatar05"),
-        video(image: "screenshot06", title: "Lijiang Lugu Lake", subTitle: "Allen - 20:30", source: "video02.mp4", avatar: "avatar06"),
-        video(image: "screenshot07", title: "Lijiang Lugu Lake", subTitle: "Allen - 20:30", source: "video02.mp4", avatar: "avatar07"),
-        video(image: "screenshot08", title: "Lijiang Lugu Lake", subTitle: "Allen - 20:30", source: "video02.mp4", avatar: "avatar08")
-    ]
-    
-    
+
     
     @IBOutlet var HomeTable: UITableView!
     override func viewDidLoad() {
---
+++ 11-day/11-day/data.swift
@@ -0,0 +1,23 @@
+//
+//  data.swift
+//  11-day
+//
+//  Created by Hongbo Yu on 16/3/25.
+//  Copyright © 2016年 Frank. All rights reserved.
+//
+
+import UIKit
+
+
+var data :[video] = [
+    video(image: "screenshot01", title: "Introduce 3DS Mario", subTitle: "Youtube - 06:32", source: "video01.mp4", avatar: "avatar01"),
+    video(image: "screenshot02", title: "Emoji Among Us", subTitle: "Vimeo - 3:34", source: "video01.mp4", avatar: "avatar02"),
+    video(image: "screenshot03", title: "Seals Documentary", subTitle: "Vine - 00:06", source: "video01.mp4", avatar: "avatar03"),
+    video(image: "screenshot04", title: "Adventure Time", subTitle: "Youtube - 02:39", source: "video01.mp4", avatar: "avatar04"),
+    video(image: "screenshot05", title: "Facebook HQ", subTitle: "Facebook - 10:20", source: "video02.mp4", avatar: "avatar05"),
+    video(image: "screenshot06", title: "Lijiang Lugu Lake", subTitle: "Allen - 20:30", source: "video02.mp4", avatar: "avatar06"),
+    video(image: "screenshot07", title: "Lijiang Lugu Lake", subTitle: "Allen - 20:30", source: "video02.mp4", avatar: "avatar07"),
+    video(image: "screenshot08", title: "Lijiang Lugu Lake", subTitle: "Allen - 20:30", source: "video02.mp4", avatar: "avatar08")
+]
+
+
