ae4d462659790a7eeb965bda87fae04bd97d9082
adolfrank <adolfrank@outlook.com>
2016-04-19 09:59:44 +0800

    完善功能

--- 17-day/17-day/AppDelegate.swift
+++ 17-day/17-day/AppDelegate.swift
@@ -12,13 +12,66 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var mask: CALayer?
+    var imageView: UIImageView?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        
+        let story = UIStoryboard(name: "Main", bundle: nil)
+        let VC: NavViewController = story.instantiateViewControllerWithIdentifier("firstVC") as! NavViewController
+        self.window?.rootViewController = VC
+    
+        
+        let imageView = UIImageView(frame: self.window!.frame)
+        imageView.image = UIImage(named: "splash")
+        VC.view.insertSubview(imageView, atIndex: 10)
+        
+        self.mask = CALayer()
+        self.mask!.contents = UIImage(named: "logo")?.CGImage
+//        self.mask?.backgroundColor = UIColor.redColor().CGColor
+        self.mask!.contentsGravity = kCAGravityResizeAspect
+        self.mask!.bounds = CGRect(x: 0, y: 0, width: 100, height: 81)
+        self.mask!.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        self.mask!.position = CGPoint(x: imageView.frame.size.width / 2, y: imageView.frame.size.height / 2)
+        imageView.layer.mask = mask
+        self.imageView = imageView
+        
+        animateMask()
+       
+        self.window!.makeKeyAndVisible()
+        
         return true
     }
 
+    
+    
+    func animateMask() {
+        
+        let keyFrameAnimation = CAKeyframeAnimation(keyPath: "bounds")
+        keyFrameAnimation.delegate = self
+        keyFrameAnimation.duration = 0.5
+        keyFrameAnimation.beginTime = CACurrentMediaTime() + 0.5
+        keyFrameAnimation.timingFunctions = [CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut), CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)]
+        let initalBounds = NSValue(CGRect: mask!.bounds)
+        let secondBounds = NSValue(CGRect: CGRect(x: 0, y: 0, width: 90, height: 73))
+        let finalBounds = NSValue(CGRect: CGRect(x: 0, y: 0, width: 2400, height: 2000))
+        keyFrameAnimation.values = [initalBounds, secondBounds, finalBounds]
+        keyFrameAnimation.keyTimes = [0, 0.3, 1]
+        self.mask!.addAnimation(keyFrameAnimation, forKey: "bounds")
+        
+    }
+    
+    override func animationDidStop(anim: CAAnimation, finished flag: Bool) {
+        self.imageView!.layer.mask = nil
+        self.imageView?.removeFromSuperview()
+        
+    }
+
+    
+    
+    
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
---
+++ 17-day/17-day/NavViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NavViewController.swift
+//  17-day
+//
+//  Created by Hongbo Yu on 16/4/18.
+//  Copyright © 2016年 Frank. All rights reserved.
+//
+
+import UIKit
+
+class NavViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
